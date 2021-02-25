e2da295d507015c345c9a14480194c29a8c30be0
Maksym Skliarov <maksym.skliarov@gmail.com>
2021-02-25 23:04:25 +0200

    Made deployment work post-merge

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,10 +2,10 @@ class SentryJob < ActiveJob::Base
   queue_as :default
 
   def perform(event, hint)
-    Sentry.send_event(event, hint)
+    # Sentry.send_event(event, hint)
   end
 end
 
-Sentry.init do |config|
-  config.async = ->(event, hint) { SentryJob.perform_later(event, hint) }
-end
+# Sentry.init do |config|
+#   config.async = ->(event, hint) { SentryJob.perform_later(event, hint) }
+# end
